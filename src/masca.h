1e4670cf0cb819debeef486a3742ebce3a6e71fc
salinasv <salinasv@gmail.com>
2008-08-07 13:33:04 -0500

    Add masca.h with some useful declarations

---
+++ src/masca.h
@@ -0,0 +1,16 @@
+#ifndef _MASCA_H_
+#define _MASCA_H_
+
+/* Util constants */
+#ifndef NULL
+#define NULL 0
+#endif /* NULL */
+
+/* Boolean */
+typedef char bool;
+#define true 1
+#define TRUE 1
+#define false 0
+#define FALSE 0
+
+#endif /* _MASCA_H_ */
