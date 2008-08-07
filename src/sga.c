015826a98c2a9054e250c69cd889fd6a1433b3a1
salinasv <salinasv@gmail.com>
2008-08-07 18:32:19 -0500

    Add the main files to the repo

---
+++ src/sga.c
@@ -0,0 +1,21 @@
+#include <sdtio.h>
+
+/*
+ * Here you are suppoused to set all the parameters to be used by the AG
+ */
+void init_GA()
+{
+}
+
+/*
+ * Here you are suppoused to select wich operator do you want to use
+ */
+void runGA()
+{
+}
+
+int main(int argc, char *argv[])
+{
+	init_GA();
+	run_GA();
+}
---
+++ src/sga.h
@@ -0,0 +1,9 @@
+#ifndef _SGA_H_
+#define _SGA_H_
+
+enum
+{
+	SGA_PROBLEM_TSP;
+} SgaProblem;
+
+#endif /* _SGA_H_ */
