7c6a69414a4b109cf0c9f2b6bf7dbfd0d5149c23
salinasv <salinasv@gmail.com>
2008-08-17 23:03:26 -0500

    Added a populationwise crossover functions. Also renamed the previous ones to get it intuitive.

--- src/cross.c
+++ src/cross.c
@@ -3,7 +3,7 @@
 #include "cross.h"
 
 
-void cross_point_one(const Individual *parent1, const Individual *parent2,
+void cross_point_one_ind(const Individual *parent1, const Individual *parent2,
 		Individual *son1, Individual *son2,
 		unsigned int crosspoint, unsigned int gen_num, double crossprob)
 {
@@ -19,7 +19,7 @@ void cross_point_one(const Individual *parent1, const Individual *parent2,
 	individual_cpy_chrom_first(son2, parent1, crosspoint, gen_num, FALSE);
 }
 
-void cross_point_two(const Individual *parent1, const Individual *parent2,
+void cross_point_two_ind(const Individual *parent1, const Individual *parent2,
 		Individual *son1, Individual *son2,
 		unsigned int crosspoint1, unsigned int crosspoint2,
 		unsigned int gen_num, double crossprob)
@@ -36,4 +36,48 @@ void cross_point_two(const Individual *parent1, const Individual *parent2,
 	individual_cpy_chrom_first(son2, parent2, crosspoint1, gen_num, TRUE);
 	individual_cpy_chrom_first(son2, parent1, crosspoint1, gen_num, FALSE);
 	individual_cpy_chrom_first(son2, parent2, crosspoint2, gen_num, FALSE);
+}
+
+void cross_point_one(Population *pop, double crossprob)
+{
+	double norm_cross;
+	unsigned int point;
+	int i;
+
+	norm_cross = crossprob * RAND_MAX;
+
+	srand(pop);
+
+	for (i = 0; i < pop->actual_size; i += 2) {
+		if (rand() >= norm_cross)
+			continue;
+
+		point = rand() % pop->actual_size;
+		cross_point_one_ind(pop->individuals[i], pop->individuals[i+1],
+				pop->mating_pool[i], pop->mating_pool[i+1],
+				point, pop->ind_gen_num);
+	}
+}
+
+void cross_point_two(Population *pop, double crossprob)
+{
+	double norm_cross;
+	unsigned int point;
+	unsigned int point2;
+	int i;
+
+	norm_cross = crossprob * RAND_MAX;
+
+	srand(pop);
+
+	for (i = 0; i < pop->actual_size; i += 2) {
+		if (rand() >= norm_cross)
+			continue;
+
+		point = rand() % pop->actual_size;
+		point2 = rand() % pop->actual_size;
+		cross_point_one_ind(pop->individuals[i], pop->individuals[i+1],
+				pop->mating_pool[i], pop->mating_pool[i+1],
+				point, point2, pop->ind_gen_num);
+	}
 }
--- src/cross.h
+++ src/cross.h
@@ -4,6 +4,22 @@
 #include "individual.h"
 
 /**
+ * Perform the one point crossover operator over a entire population
+ *
+ * @param pop		The population
+ * @param crossprob	The probability to perform the cross
+ */
+void cross_point_one(Population *pop, double crossprob);
+
+/**
+ * Perform the two points crossover operator over a entire population
+ *
+ * @param pop		The population
+ * @param crossprob	The probability to perform the cross
+ */
+void cross_point_two(Population *pop, double crossprob);
+
+/**
  * Perform a One Point crossover ver the two parents setting them on the two
  * sons
  *
@@ -14,7 +30,7 @@
  * @param crosspoint	The point where to perform the cross
  * @param crossprob		The cross probability
  */
-void cross_point_one(const Individual *parent1, const Individual *parent2,
+void cross_point_one_ind(const Individual *parent1, const Individual *parent2,
 		Individual *son1, Individual *son2,
 		unsigned int crosspoint, unsigned int gen_num, double crossprob);
 
@@ -30,7 +46,7 @@ void cross_point_one(const Individual *parent1, const Individual *parent2,
  * @param crosspoint2	The second point where to performe the cross
  * @param crossprob		The cross probability
  */
-void cross_point_two(const Individual *parent1, const Individual *parent2,
+void cross_point_two_ind(const Individual *parent1, const Individual *parent2,
 		Individual *son1, Individual *son2,
 		unsigned int crosspoint1, unsigned int crosspoint2,
 		unsigned int gen_num, double crossprob);
