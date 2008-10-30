b62d5405fcb4e7cb46c85564b8e5dd67b0bfb3bc
salinasv <salinasv@gmail.com>
2008-10-30 00:02:17 -0600

    initial driver test code. Let's see if it works

---
+++ driver/haga_drvr.c
@@ -0,0 +1,20 @@
+/*
+ * HAGA driver
+ *
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+ 
+MODULE_ICENSE("GPL");
+
+static int haga_init(void)
+{
+}
+
+static void haga_exit(void)
+{
+}
+
+module_init(haga_init);
+module_exit(haga_exit);
