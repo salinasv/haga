512fcc82520e3a22dade7dd3dff5f312fd3230f6
salinasv <salinasv@gmail.com>
2008-11-02 00:16:16 -0600

    Add debug level stuff.

--- driver/haga_drvr.c
+++ driver/haga_drvr.c
@@ -5,16 +5,26 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+
+#define LCL_DEBUG_ALL 0
+
+#define LCL_DEBUG_LEVEL LCL_DEBUG_ALL
  
 MODULE_LICENSE("GPL");
 
 static int __init haga_init(void)
 {
+#if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
+	printk(KERN_ALERT "Loading haga-drvr\n");
+#endif /* LCL_DEBUG_LEVEL */
 	return 0;
 }
 
 static void __exit haga_exit(void)
 {
+#if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
+	printk(KERN_ALERT "Unloading haga-drvr\n");
+#endif /* LCL_DEBUG_LEVEL */
 }
 
 module_init(haga_init);
