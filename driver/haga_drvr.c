587ed71930af0858a2e328fd516edeab24d25d85
salinasv <salinasv@gmail.com>
2008-11-05 00:03:39 -0600

    Create the char device and register it.

--- driver/haga_drvr.c
+++ driver/haga_drvr.c
@@ -3,6 +3,7 @@
  *
  */
 
+#include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -14,6 +15,11 @@
 
 #define PCI_DEVICE_ID_TSP_COP 0x5050
 
+/* Char device info */
+#define HAGA_MAJOR 1000
+#define HAGA_MINOR 0
+#define HAGA_DEV_COUNT 1
+
 MODULE_LICENSE("GPL");
 
 static struct pci_device_id ids[] = {
@@ -22,6 +28,11 @@ static struct pci_device_id ids[] = {
 };
 MODULE_DEVICE_TABLE(pci, ids);
 
+int haga_major = HAGA_MAJOR;
+int haga_minor = HAGA_MINOR;
+
+struct cdev *cdevice;
+ 
 /* File operaitons */
 int haga_open(struct inode *node, struct file *flip)
 {
@@ -67,18 +78,49 @@ static struct file_operations haga_fop = {
 
 static int __init haga_init(void)
 {
+	int result;
+	dev_t dev;
+
 #if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
 	printk(KERN_ALERT "Loading haga-drvr.\n");
 #endif /* LCL_DEBUG_LEVEL */
+
+	if (haga_major) {
+		dev = MKDEV(haga_major, haga_minor);
+		result = register_chrdev_region(dev, HAGA_DEV_COUNT, "haga");
+	} else {
+		result = alloc_chrdev_region(&dev , haga_minor, HAGA_DEV_COUNT, "haga");
+		haga_major = MAJOR(dev);
+	}
+	if (result < 0) {
+		printk(KERN_WARNING "haga: can't get major %d.\n", haga_major);
+		return result;
+	}
+
+	cdevice = cdev_alloc();
+	cdevice->owner = THIS_MODULE;
+	cdevice->ops = &haga_fop;
+
+	dev = MKDEV(haga_major, haga_minor);
+	result = cdev_add(cdevice, dev, 1);
+
+	if (result)
+		printk(KERN_NOTICE "Error %d adding haga.\n", result);
 
 	return pci_register_driver(&pci_driver);
 }
 
 static void __exit haga_exit(void)
 {
+	dev_t dev;
+
 #if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
 	printk(KERN_ALERT "Unloading haga-drvr.\n");
 #endif /* LCL_DEBUG_LEVEL */
+
+	dev = MKDEV(haga_major, haga_minor);
+	unregister_chrdev_region(dev, HAGA_DEV_COUNT);
+	cdev_del(cdevice);
 
 	pci_unregister_driver(&pci_driver);
 }
