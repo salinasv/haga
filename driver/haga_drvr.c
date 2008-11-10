74204dadd96098f95f976130b501d6191c989ce0
salinasv <salinasv@gmail.com>
2008-11-10 00:23:46 -0600

    As we are building this with 2.6 we can use misc_register family instead of chrdev to make it easy. Code looks clean now.

--- driver/haga_drvr.c
+++ driver/haga_drvr.c
@@ -3,9 +3,10 @@
  *
  */
 
-#include <linux/cdev.h>
+//#include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/pci.h>
 
@@ -15,11 +16,6 @@
 
 #define PCI_DEVICE_ID_TSP_COP 0x5050
 
-/* Char device info */
-#define HAGA_MAJOR 1000
-#define HAGA_MINOR 0
-#define HAGA_DEV_COUNT 1
-
 MODULE_LICENSE("GPL");
 
 static struct pci_device_id ids[] = {
@@ -27,9 +23,6 @@ static struct pci_device_id ids[] = {
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, ids);
-
-int haga_major = HAGA_MAJOR;
-int haga_minor = HAGA_MINOR;
 
 struct cdev *cdevice;
  
@@ -76,52 +69,35 @@ static struct file_operations haga_fop = {
 	.release = 	haga_release,
 };
 
+static struct miscdevice haga_misc_device = {
+	.minor 	= MISC_DYNAMIC_MINOR,
+	.name 	= "haga",
+	.fops 	= &haga_fop,
+};
+
 static int __init haga_init(void)
 {
 	int result;
-	dev_t dev;
 
 #if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
 	printk(KERN_ALERT "Loading haga-drvr.\n");
 #endif /* LCL_DEBUG_LEVEL */
 
-	if (haga_major) {
-		dev = MKDEV(haga_major, haga_minor);
-		result = register_chrdev_region(dev, HAGA_DEV_COUNT, "haga");
-	} else {
-		result = alloc_chrdev_region(&dev , haga_minor, HAGA_DEV_COUNT, "haga");
-		haga_major = MAJOR(dev);
+	result = misc_register(&haga_misc_device);
+	if (result) {
+		printk(KERN_ERR "misc_register failed with error code %d.\n", result);
 	}
-	if (result < 0) {
-		printk(KERN_WARNING "haga: can't get major %d.\n", haga_major);
-		return result;
-	}
-
-	cdevice = cdev_alloc();
-	cdevice->owner = THIS_MODULE;
-	cdevice->ops = &haga_fop;
-
-	dev = MKDEV(haga_major, haga_minor);
-	result = cdev_add(cdevice, dev, 1);
-
-	if (result)
-		printk(KERN_NOTICE "Error %d adding haga.\n", result);
 
 	return pci_register_driver(&pci_driver);
 }
 
 static void __exit haga_exit(void)
 {
-	dev_t dev;
 
 #if LCL_DEBUG_LEVEL >= LCL_DEBUG_ALL
 	printk(KERN_ALERT "Unloading haga-drvr.\n");
 #endif /* LCL_DEBUG_LEVEL */
-
-	dev = MKDEV(haga_major, haga_minor);
-	unregister_chrdev_region(dev, HAGA_DEV_COUNT);
-	cdev_del(cdevice);
-
+	misc_deregister(&haga_misc_device);
 	pci_unregister_driver(&pci_driver);
 }
 
